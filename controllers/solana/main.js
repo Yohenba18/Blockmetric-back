62cf76f31270ef26ed390314849aa5ff59d8f021
Yohenba18 <roddik12kshetri@gmail.com>
2021-12-01 10:40:45 +0530

    contributors

--- controllers/solana/main.js
+++ controllers/solana/main.js
@@ -1,13 +1,34 @@
-const solanaWeb3 = require("@solana/web3.js");
+const web3 = require("@solana/web3.js");
 
-const connection = new solanaWeb3.Connection(
-  solanaWeb3.clusterApiUrl("mainnet-beta"),
+const connection = new web3.Connection(
+  web3.clusterApiUrl("mainnet-beta"),
   "confirmed"
 );
+// mainnet-beta
 
 const getSolanaData = async (req, res) => {
   try {
-    await console.log(connection.getBlockTime(1000));
+    // var wallet = web3.Keypair.generate();
+    // var airdropSignature = await connection.requestAirdrop(
+    //   wallet.publicKey,
+    //   web3.LAMPORTS_PER_SOL
+    // );
+    // console.log(wallet);
+    // await connection.confirmTransaction(airdropSignature);
+    // let account = await connection.getAccountInfo(wallet.publicKey);
+    // console.log(account);
+    let slot = await connection.getSlot();
+    console.log(slot);
+    // 93186439
+
+    let blockTime = await connection.getBlockTime(slot);
+    console.log(blockTime);
+    // // 1630747045
+    console.log(tx.serializeMessage().toString("base64"));
+
+    // let block = await connection.getBlock(slot);
+    // console.log(block);
+
     res.status(200).json("working!!!");
   } catch (error) {
     console.log(error);
--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@
   1. npm i 
   2. npm start
 ```
+
+### Contributors:-
+
+[![](https://opencollective.com/html-react-parser/contributors.svg?width=890&button=false)](https://github.com/remarkablemark/html-react-parser/graphs/contributors)

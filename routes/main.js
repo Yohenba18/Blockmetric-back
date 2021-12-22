cd92400e59ca9a9a599608c6095da6508591b647
Yohenba18 <roddik12kshetri@gmail.com>
2021-12-22 10:21:35 +0530

    database connection done

--- routes/main.js
+++ routes/main.js
@@ -7,10 +7,10 @@ const { getSolana } = require("../controllers/getchains/solana");
 const { getRipple } = require("../controllers/getchains/xrpl");
 const { getTezos } = require("../controllers/getchains/tezos");
 
-const { getAllData } = require("../controllers/alldata");
-const { getAllTransactions } = require("../controllers/alltransactions");
-const { getAllDevelopers } = require("../controllers/alldevelopers");
-const { getAllPrice } = require("../controllers/allprices");
+// const { getAllData } = require("../controllers/alldata");
+// const { getAllTransactions } = require("../controllers/alltransactions");
+// const { getAllDevelopers } = require("../controllers/alldevelopers");
+// const { getAllPrice } = require("../controllers/allprices");
 
 const {
   DbAllData,
@@ -20,7 +20,7 @@ const {
 } = require("../controllers/DbData");
 
 //route to get the entire data
-//* need to change this
+//* route changed to DbData.js
 router.route("/").get(DbAllData);
 
 //seperate routes for each chain
@@ -32,7 +32,7 @@ router.route("/Tezos").get(getTezos);
 router.route("/Ripple").get(getRipple);
 
 //routes to get specific data of all chains
-//* need to change this
+//* routes changed to DbData.js
 router.route("/Transaction").get(DbAllTransactionsData);
 router.route("/Developers").get(DbAlDeveloperslData);
 router.route("/Price").get(DbAllPricesData);
